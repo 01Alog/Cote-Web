f60f8e67adcb80c981bdbd1c6e0eaba65fc29de4
JoaquinT <joaquintorresv2005@gmail.com>
2024-10-06 00:06:42 -0300

    cambio de fondo

--- components/Home.tsx
+++ components/Home.tsx
@@ -8,18 +8,18 @@ import {
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-black shadow-lg rounded-lg p-6 max-w-sm w-full">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-200 to-pink-200">
+      <div className="p-8 bg-white shadow-lg rounded-lg transform transition-all duration-500 hover:scale-105">
         <div className="flex flex-col items-center">
           <img
             className="rounded-full w-32 h-32 object-cover"
-            src="/images/img2.png" // Reemplaza con la ruta de tu imagen
+            src="/images/img2.png" // Reemplaza con la ruta correcta de tu imagen
             alt="Profile"
           />
-          <h2 className="text-white text-xl font-semibold mt-4">
+          <h2 className="text-gray-900 text-xl font-semibold mt-4">
             Maria Jose Palmas Salinas
           </h2>
-          <p className="text-gray-300 mt-2">
+          <p className="text-gray-600 mt-2">
             Descripción breve o información adicional aquí.
           </p>
           <div className="flex space-x-4 mt-4">
@@ -29,7 +29,7 @@ const Home = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaInstagram className="text-white text-2xl hover:text-gray-400 transition-colors" />
+              <FaInstagram className="text-gray-900 text-2xl hover:text-gray-600 transition-colors" />
             </a>
             <a
               href="https://wa.link/vkhy3c"
@@ -37,7 +37,7 @@ const Home = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaWhatsapp className="text-white text-2xl hover:text-gray-400 transition-colors" />
+              <FaWhatsapp className="text-gray-900 text-2xl hover:text-gray-600 transition-colors" />
             </a>
             <a
               href="https://www.facebook.com/maria.j.palma.12"
@@ -45,7 +45,7 @@ const Home = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaFacebook className="text-white text-2xl hover:text-gray-400 transition-colors" />
+              <FaFacebook className="text-gray-900 text-2xl hover:text-gray-600 transition-colors" />
             </a>
             <a
               href="https://linkedin.com"
@@ -53,7 +53,7 @@ const Home = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaLinkedin className="text-white text-2xl hover:text-gray-400 transition-colors" />
+              <FaLinkedin className="text-gray-900 text-2xl hover:text-gray-600 transition-colors" />
             </a>
           </div>
         </div>
